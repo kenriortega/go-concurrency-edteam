5565173a172575a119d2764bcdfe8a1bfdcb0509
kenriortega <kenriortega@gmail.com>
2021-08-22 12:27:44 -0400

    pattern for select

--- moduls3/chanbasic.go
+++ moduls3/chanbasic.go
@@ -3,27 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	num := make(chan int, 2)
+	num := make(chan int)
 	signal := make(chan struct{})
 	go receive(signal, num)
 	send(num)
 
-	<-signal
+	signal <- struct{}{}
+
 }
 
 func send(num chan<- int) {
 	num <- 1
 	num <- 2
 	num <- 3
-	close(num)
+	// close(num)
 }
 
-func receive(signal chan<- struct{}, num <-chan int) {
-	for v := range num {
-		// v, ok := <-num
-		// if ok {
-		fmt.Printf("%d\n", v)
-		// }
+func receive(signal <-chan struct{}, num <-chan int) {
+	// for v := range num {
+	// 	// v, ok := <-num
+	// 	// if ok {
+	// 	fmt.Printf("%d\n", v)
+	// 	// }
+	// }
+	for {
+		select {
+		case v := <-num:
+			fmt.Println(v)
+		case <-signal:
+			return
+		default:
+			fmt.Println("as")
+		}
 	}
-	signal <- struct{}{}
 }
